8148912db2fadec14509b29f1cfe015d42165273
sbx_user1051 <sbx_user1051@169.254.16.89>
2025-01-31 15:54:37 +0000

    Fri Jan 31 15:54:37 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,31 +18,25 @@ def checkout(skus: str) -> int:
 
     # in a first-pass, remove items from the basket which are part of a freebie offer
     for id, count in basket.items():
-        if id in FREEBIES:
+        if id in FREEBIES and FREEBIES[id][1] in basket:
             required_count, free_id = FREEBIES[id]
-            basket[free_id] / 
-            while count >= required_count and basket_copy.get(free_id, 0) > 0:
-                total -= ITEMS[free_id]
-                basket[free_id] -= 1
-
+            x = int(basket[free_id] / required_count)
+            basket[free_id] = max(basket[free_id] - x, 0)
 
     # calculate the basket value
     total = 0
     for id, count in basket.items():
+        _calculate_item_basket_price
         # for items without offers, we can simply add the price muliplied by count
         unit_price = ITEMS[id]
         if id not in OFFERS:
             total += unit_price * count
         else:
-            # for items with offers, factor in the reduced price based on the number of offers
-            offer_req, offer_price = OFFERS[id][0]  # TODO: pick best offer
-            
+            # for items with offers, factor in the reduced price based on the b
+            offer_req, offer_price = OFFERS[id][0]  # TODO: assume best offer
+
             total += int(count / offer_req) * offer_price
             total += count % offer_req * unit_price
-
-        # for items with freebies, remove their value from the total if present in the basket
-        if id in FREEBIES:
-
 
     return total
 
@@ -58,6 +52,7 @@ def _build_basket(skus: str) -> dict[str, int]:
         basket[id] += 1
 
     return basket
+
 
 
 
