ae9cb72d3b60e82d3ed99867ebed99207872ed8f
sbx_user1051 <sbx_user1051@169.254.16.89>
2025-01-31 18:07:14 +0000

    Fri Jan 31 18:07:14 UTC 2025

--- lib/solutions/CHK/sku_data.py
+++ lib/solutions/CHK/sku_data.py
@@ -8,7 +8,8 @@ class SkuData:
         self.item_prices: dict[str, int] = {}
         self.offers: dict[str, list[tuple]] = defaultdict(list)
         self.freebies: dict[str, tuple] = defaultdict(list)
-
+        self.multi_offers: list[tuple[int, list[str]]] = []
+        
         for line in price_table.splitlines()[3:-1]:
             l = line.split("|")
             item = l[1].strip()
@@ -18,7 +19,9 @@ class SkuData:
 
             for offer in offers.split(", "):
                 if "buy any " in offer:
-                    
+                    # buy any 3 of (S,T,X,Y,Z) for 45
+                    pass
+                    # self.multi_offers.append()
                 elif " for " in offer:
                     for_offer = offer.split(" for ")
                     item_id = for_offer[0][-1]
@@ -47,5 +50,6 @@ class SkuData:
 
 
 sku_data = SkuData(PRICE_TABLE)
+
 
 
