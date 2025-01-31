2620fe4f49736c3146d75e3c857d75970d48f368
sbx_user1051 <sbx_user1051@169.254.16.89>
2025-01-31 16:32:36 +0000

    Fri Jan 31 16:32:36 UTC 2025

--- lib/solutions/CHK/sku_data.py
+++ lib/solutions/CHK/sku_data.py
@@ -1,3 +1,38 @@
+PRICE_TABLE = """+------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
+| F    | 10    | 2F get one F free      |
+| G    | 20    |                        |
+| H    | 10    | 5H for 45, 10H for 80  |
+| I    | 35    |                        |
+| J    | 60    |                        |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
++------+-------+------------------------+"""
+
+class SkuData:
+    def __init__(self, price_table: str):
+        pass
+
 ITEMS: dict[str, int] = {
     "A": 50,
     "B": 30,
@@ -16,3 +51,4 @@ FREEBIES: dict[str, tuple] = {
     "E": (2, "B"),
     "F": (3, "F"),
 }
+
