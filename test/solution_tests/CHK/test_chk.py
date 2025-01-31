64a4cb677254d2f6e2e17a56720e681f78e0ab2e
sbx_user1051 <sbx_user1051@169.254.16.89>
2025-01-31 14:57:07 +0000

    Fri Jan 31 14:57:07 UTC 2025

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -16,3 +16,18 @@ class TestSum:
     def test_checkout_one_item(self):
         assert checkout_solution.checkout("A") == 50
         assert checkout_solution.checkout("C") == 15
+
+    def test_checkout_multiple_items_no_offer(self):
+        assert checkout_solution.checkout("AC") == 70
+        assert checkout_solution.checkout("ABCD") == 115
+    
+    def test_checkout_multiple_items_one_offer(self):
+        assert checkout_solution.checkout("AA") == 70
+
+    def test_checkout_multiple_items_multiple_offer(self):
+        assert checkout_solution.checkout("AC") == 70
+
+    def test_checkout_multiple_items_repeat_offer(self):
+
+    def test_checkout_illegal_input(self):
+
