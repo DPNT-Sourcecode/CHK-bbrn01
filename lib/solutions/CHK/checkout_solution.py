50d9f1a1346dcd822816b3e869560f2fd5f09a2e
sbx_user1051 <sbx_user1051@169.254.16.89>
2025-01-31 15:39:37 +0000

    Fri Jan 31 15:39:37 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,18 +18,22 @@ def checkout(skus: str) -> int:
 
     # calculate the basket value
     total = 0
+    basket_copy = basket.copy()
     for id, count in basket.items():
         # for items without offers, we can simply add the price muliplied by count
         unit_price = ITEMS[id]
         if id not in OFFERS:
             total += unit_price * count
-            continue
+        else:
+            # for items with offers, factor in the reduced price based on the number of offers
+            offer_req, offer_price = OFFERS[id][0]  # TODO: pick best offer
+            
+            total += int(count / offer_req) * offer_price
+            total += count % offer_req * unit_price
 
-        # for items with offers, factor in the reduced price based on the number of offers
-        offer_req, offer_price = OFFERS[id][0]  # TODO: pick best offer
-        
-        total += int(count / offer_req) * offer_price
-        total += count % offer_req * unit_price
+        # for items with freebies, remove their value from the total if present in the basket
+        if id in FREEBIES:
+            
 
     return total
 
@@ -45,3 +49,4 @@ def _build_basket(skus: str) -> dict[str, int]:
         basket[id] += 1
 
     return basket
+
