f6142e18e72625e88cf5b794975b0a2fdaceeea5
sbx_user1051 <sbx_user1051@169.254.16.89>
2025-01-31 15:03:07 +0000

    Fri Jan 31 15:03:07 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,22 @@
+# +------+-------+----------------+
+# | Item | Price | Special offers |
+# +------+-------+----------------+
+# | A    | 50    | 3A for 130     |
+# | B    | 30    | 2B for 45      |
+# | C    | 20    |                |
+# | D    | 15    |                |
+# +------+-------+----------------+
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
-    raise NotImplementedError()
+    # 
+    if not isinstance(skus, str):
+        return -1
+    for item in skus:
+
+    
 
 
 
