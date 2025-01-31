eaf827b86bd3ab8842483efa3aef0cc0cc13cf83
sbx_user1051 <sbx_user1051@169.254.16.89>
2025-01-31 14:44:59 +0000

    Fri Jan 31 14:44:59 UTC 2025

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -3,4 +3,5 @@ from solutions.HLO import hello_solution
 
 class TestSum:
     def test_hello(self):
-        assert hello_solution.hello(1, 2) == 3
+        assert hello_solution.hello("placeholder") == "Hello, World!"
+
